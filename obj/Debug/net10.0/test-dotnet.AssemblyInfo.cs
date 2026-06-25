4adefcf35c3c7770ef66d03da2967db117f0deea
Yuta1409 <yroger2003@gmail.com>
2026-06-25 14:03:31 +0200

    fichier debug

--- obj/Debug/net10.0/test-dotnet.AssemblyInfo.cs
+++ obj/Debug/net10.0/test-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f96f578d5d4cf4137e1afea7be911acfb05bf62")]
 [assembly: System.Reflection.AssemblyProductAttribute("test-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
